9bcd0f284fbc83e097bbb95d293513c7ca1d42dd
satyam1509 <patel1996satyam@gmail.com>
2022-09-05 11:32:10 +0530

    final

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-console.log("hello");
-
 function tempConvert(xyz) {
-  console.log(xyz)
   let Temp = document.getElementById("in")?.value;
-
-if(Temp===""){
+  if (Temp === "") {
     document.getElementById("result").innerText = "Please Enter Any Value!";
-}else{
-  (Temp=='celcius')?document.getElementById("result").innerText = ((Temp - 32) * 5) / 9 + "\xB0C":
-  document.getElementById("result").innerText = (Temp * 9) / 5 + 32 + "\xB0F";
+  } else {
+    (xyz == "celcius")
+      ? (document.getElementById("result").innerText =
+          Math.round(((Temp - 32) * 5) / 9) + "\xB0C")
+      : (document.getElementById("result").innerText =
+          Math.round((Temp * 9) / 5 + 32) + "\xB0F");
+  }
 }
-}
-
-// function toFahrenheit() {
-    // let Temp = document.getElementById("in").value;
-    // if(Temp===""){
-        // document.getElementById("result").innerText = "Please Enter Any Value!";
-    // }else{
-
-
-// }
-// }
